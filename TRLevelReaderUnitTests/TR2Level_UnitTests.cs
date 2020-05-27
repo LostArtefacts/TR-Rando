3b2089bfb7933eceba0c4c7a882b2289ee6568c6
DanzaG <360danza@gmail.com>
2020-05-27 21:12:24 +0100

    #6 - Basic project setup. Level reader will focus on TR2 only for now.

---
+++ TRLevelReaderUnitTests/TR2Level_UnitTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TRLevelReaderUnitTests
+{
+    [TestClass]
+    public class TR2Level_UnitTests
+    {
+        [TestMethod]
+        public void Placeholder()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
