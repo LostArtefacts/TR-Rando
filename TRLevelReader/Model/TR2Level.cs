b752b6467138b98402b1aebb86447c2a1cefd0e2
DanzaG <360danza@gmail.com>
2020-05-27 21:50:40 +0100

    #6 - Started converting struct to properties for TR2 Level object.

--- TRLevelReader/Model/TR2Level.cs
+++ TRLevelReader/Model/TR2Level.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,67 +11,75 @@ namespace TRLevelReader.Model
 {
     public class TR2Level
     {
-        /*
-            uint32_t Version; // version (4 bytes)
-            tr_colour Palette[256]; // 8-bit palette (768 bytes)
-            tr_colour4 Palette16[256]; //  (1024 bytes)
-            uint32_t NumImages; // number of texture images (4 bytes)
-            tr_teximage8 Images8[NumImages]; // 8-bit (palettized) images (NumImages * 65536 bytes)
-            tr_teximage16 Images16[NumImages]; // 16-bit (ARGB) images (NumImages * 131072 bytes)
-            uint32_t Unused; // 32-bit unused value (4 bytes)
-            uint16_t NumRooms; // number of rooms (2 bytes)
-            tr2_room Rooms[NumRooms]; // room list (variable length)
-            uint32_t NumFloorData; // number of floor data uint16_t's to follow (4 bytes)
-            uint16_t FloorData[NumFloorData]; // floor data (NumFloorData * 2 bytes)
-            uint32_t NumMeshData; // number of uint16_t's of mesh data to follow (=Meshes[]) (4 bytes)
-            tr_mesh Meshes[NumMeshPointers]; // note that NumMeshPointers comes AFTER Meshes[]
-            uint32_t NumMeshPointers; // number of mesh pointers to follow (4 bytes)
-            uint32_t MeshPointers[NumMeshPointers]; // mesh pointer list (NumMeshPointers * 4 bytes)
-            uint32_t NumAnimations; // number of animations to follow (4 bytes)
-            tr_animation Animations[NumAnimations]; // animation list (NumAnimations * 32 bytes)
-            uint32_t NumStateChanges; // number of state changes to follow (4 bytes)
-            tr_state_change StateChanges[NumStateChanges]; // state-change list (NumStructures * 6 bytes)
-            uint32_t NumAnimDispatches; // number of animation dispatches to follow (4 bytes)
-            tr_anim_dispatch AnimDispatches[NumAnimDispatches]; // animation-dispatch list list (NumAnimDispatches * 8 bytes)
-            uint32_t NumAnimCommands; // number of animation commands to follow (4 bytes)
-            tr_anim_command AnimCommands[NumAnimCommands]; // animation-command list (NumAnimCommands * 2 bytes)
-            uint32_t NumMeshTrees; // number of MeshTrees to follow (4 bytes)
-            tr_meshtree_node MeshTrees[NumMeshTrees]; // MeshTree list (NumMeshTrees * 4 bytes)
-            uint32_t NumFrames; // number of words of frame data to follow (4 bytes)
-            uint16_t Frames[NumFrames]; // frame data (NumFrames * 2 bytes)
-            uint32_t NumModels; // number of models to follow (4 bytes)
-            tr_model Models[NumModels]; // model list (NumModels * 18 bytes)
-            uint32_t NumStaticMeshes; // number of StaticMesh data records to follow (4 bytes)
-            tr_staticmesh StaticMeshes[NumStaticMeshes]; // StaticMesh data (NumStaticMesh * 32 bytes)
-            uint32_t NumObjectTextures; // number of object textures to follow (4 bytes)
-            tr_object_texture ObjectTextures[NumObjectTextures]; // object texture list (NumObjectTextures * 20 bytes) (after AnimatedTextures in TR3)
-            uint32_t NumSpriteTextures; // number of sprite textures to follow (4 bytes)
-            tr_sprite_texture SpriteTextures[NumSpriteTextures]; // sprite texture list (NumSpriteTextures * 16 bytes)
-            uint32_t NumSpriteSequences; // number of sprite sequences records to follow (4 bytes)
-            tr_sprite_sequence SpriteSequences[NumSpriteSequences]; // sprite sequence data (NumSpriteSequences * 8 bytes)
-            uint32_t NumCameras; // number of camera data records to follow (4 bytes)
-            tr_camera Cameras[NumCameras]; // camera data (NumCameras * 16 bytes)
-            uint32_t NumSoundSources; // number of sound source data records to follow (4 bytes)
-            tr_sound_source SoundSources[NumSoundSources]; // sound source data (NumSoundSources * 16 bytes)
-            uint32_t NumBoxes; // number of box data records to follow (4 bytes)
-            tr2_box Boxes[NumBoxes]; // box data (NumBoxes * 8 bytes)
-            uint32_t NumOverlaps; // number of overlap records to follow (4 bytes)
-            uint16_t Overlaps[NumOverlaps]; // overlap data (NumOverlaps * 2 bytes)
-            int16_t Zones[10*NumBoxes]; // zone data (NumBoxes * 20 bytes)
-            uint32_t NumAnimatedTextures; // number of animated texture records to follow (4 bytes)
-            uint16_t AnimatedTextures[NumAnimatedTextures]; // animated texture data (NumAnimatedTextures * 2 bytes)
-            uint32_t NumEntities; // number of entities to follow (4 bytes)
-            tr2_entity Entities[NumEntities]; // entity list (NumEntities * 24 bytes)
-            uint8_t LightMap[32 * 256]; // light map (8192 bytes)
-            uint16_t NumCinematicFrames; // number of cinematic frame records to follow (2 bytes)
-            tr_cinematic_frame CinematicFrames[NumCinematicFrames]; // (NumCinematicFrames * 16 bytes)
-            uint16_t NumDemoData; // number of demo data records to follow (2 bytes)
-            uint8_t DemoData[NumDemoData]; // demo data (NumDemoData bytes)
-            int16_t SoundMap[370]; // sound map (740 bytes)
-            uint32_t NumSoundDetails; // number of sound-detail records to follow (4 bytes)
-            tr_sound_details SoundDetails[NumSoundDetails]; // sound-detail list (NumSoundDetails * 8 bytes)
-            uint32_t NumSampleIndices; // number of sample indices to follow (4 bytes)
-            uint32_t SampleIndices[NumSampleIndices]; // sample indices (NumSampleIndices * 4 bytes)
-         */
+        public uint Version { get; set; }
+
+        //public TRColour[] Palette { get; set; }
+
+        //public TRColour4[] Palette16 { get; set; }
+
+        public uint NumImages { get; set; }
+
+        //public TRTexImage8[] Images8 { get; set; }
+
+        //public TRTexImage16[] Images16 { get; set; }
+
+        public uint Unused { get; set; }
+
+        public ushort NumRooms { get; set; }
+
+        //public TR2Room[] Rooms { get; set; }
+
+        public uint NumFloorData { get; set; }
+
+        public ushort[] FloorData { get; set; }
+
+        public uint NumMeshData { get; set; }
+
+        //public TRMesh[] Meshes { get; set; }
+
+        public uint NumMeshPointers { get; set; }
+
+        public uint[] MeshPointers { get; set; }
+
+        public uint NumAnimations { get; set; }
+
+        //public TRAnimation[] Animations { get; set; }
+
+        public uint NumStateChanges { get; set; }
+
+        //public TRStateChange[] StateChanges { get; set; }
+
+        public uint NumAnimDispatches { get; set; }
+
+        //public TRAnimDispatch[] AnimDispatches { get; set; }
+
+        public uint NumAnimCommands { get; set; }
+
+        //public TRAnimCommand[] AnimCommands { get; set; }
+
+        public uint NumMeshTrees { get; set; }
+
+        //public TRMeshTreeNode[] MeshTrees { get; set; }
+
+        public uint NumFrames { get; set; }
+
+        public ushort[] Frames { get; set; }
+
+        public uint NumModels { get; set; }
+
+        //public TRModel[] Models { get; set; }
+
+        public uint NumStaticMeshes { get; set; }
+
+        //public TRStaticMesh[] StaticMeshes { get; set; }
+
+        public uint NumObjectTextures { get; set; }
+
+        //public TRObjectTexture[] ObjectTextures { get; set; }
+
+        public TR2Level()
+        {
+
+        }
     }
 }
