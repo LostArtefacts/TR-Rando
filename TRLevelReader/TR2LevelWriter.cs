45563423bd8a4b8deef5dc744fbd1f2dd4f10fa9
DanzaG <360danza@gmail.com>
2020-06-06 17:31:13 +0100

    #27 - Utility class to write a .TR2 level file

---
+++ TRLevelReader/TR2LevelWriter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRLevelReader.Model;
+
+namespace TRLevelReader
+{
+    public class TR2LevelWriter
+    {
+        public TR2LevelWriter()
+        {
+
+        }
+
+        public void WriteLevelToFile(TR2Level lvl, string filepath)
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Create(filepath)))
+            {
+                writer.Write(lvl.Serialize());
+            }
+        }
+    }
+}
