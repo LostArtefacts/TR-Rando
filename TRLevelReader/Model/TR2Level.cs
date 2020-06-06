e56cac79483f2ea6c9fa28a39ef3e07169ec050b
DanzaG <360danza@gmail.com>
2020-06-06 18:11:26 +0100

    #28 - All unit tests passing, forgot to match the /= 4 to meshtreedata when writing back.

--- TRLevelReader/Model/TR2Level.cs
+++ TRLevelReader/Model/TR2Level.cs
@@ -358,7 +358,7 @@ namespace TRLevelReader.Model
                     foreach (TRAnimDispatch dispatch in AnimDispatches) { writer.Write(dispatch.Serialize()); }
                     writer.Write(NumAnimCommands);
                     foreach (TRAnimCommand cmd in AnimCommands) { writer.Write(cmd.Serialize()); }
-                    writer.Write(NumMeshTrees);
+                    writer.Write(NumMeshTrees * 4); //To get the correct number /= 4 is done during read, make sure to reverse it here.
                     foreach (TRMeshTreeNode node in MeshTrees) { writer.Write(node.Serialize()); }
                     writer.Write(NumFrames);
                     foreach (ushort frame in Frames) { writer.Write(frame); }
