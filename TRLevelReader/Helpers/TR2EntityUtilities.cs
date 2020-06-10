0d4dccf90da1f71dc6fe56102fd60c98872f9beb
DanzaG <360danza@gmail.com>
2020-06-10 20:03:20 +0100

    #36 - Added skeletons of some helpers for getting a list of certain entity types to filter the level entities.

---
+++ TRLevelReader/Helpers/TR2EntityUtilities.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRLevelReader.Model.Enums;
+
+namespace TRLevelReader.Helpers
+{
+    public static class TR2EntityUtilities
+    {
+        public static List<TR2Entities> GetListOfEnemyTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<TR2Entities> GetListOfGunAmmoTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<TR2Entities> GetListOfKeyItemTypes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
