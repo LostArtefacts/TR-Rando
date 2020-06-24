6e3cc3c24cab50f5c54723dd5df6b3314f5760b4
DanzaG <360danza@gmail.com>
2020-06-24 21:47:42 +0100

    #55 - New classes to convert pixels to 32 bits per pixel ready for texture exporting

---
+++ TRLevelReader/Helpers/TextileToBitmapConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRLevelReader.Helpers
+{
+    internal static class TextileToBitmapConverter
+    {
+        internal static byte To32BPP(byte input)
+        {
+            return Convert.ToByte((input / 31) * (255));
+        }
+
+        internal static byte From32BPP(byte input)
+        {
+            return Convert.ToByte((input / 255) * (31));
+        }
+    }
+}
--- TRLevelReader/Model/TRTexImage16.cs
+++ TRLevelReader/Model/TRTexImage16.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TRLevelReader.Helpers;
 using TRLevelReader.Serialization;
 
 namespace TRLevelReader.Model
@@ -48,6 +49,92 @@ namespace TRLevelReader.Model
             }
 
             return sb.ToString();
+        }
+    }
+
+    //256 * 256
+    //1-bit transparency (0 = transparent, 1 = opaque) (0x8000)
+    //5-bit red channel(0x7C00)
+    //5-bit green channel(0x03E0)
+    //5-bit blue channel(0x001F)
+    public class Textile16Pixel
+    {
+        public ushort Value { get; set; }
+
+        private byte _Red 
+        { 
+            get
+            {
+                return Convert.ToByte(Value & 0x7C00);
+            }
+        }
+
+        public byte Red
+        {
+            get
+            {
+                return TextileToBitmapConverter.To32BPP(_Red);
+            }
+        }
+
+        private byte _Blue 
+        { 
+            get
+            {
+                return Convert.ToByte(Value & 0x001F);
+            }
+        }
+
+        public byte Blue
+        {
+            get
+            {
+                return TextileToBitmapConverter.To32BPP(_Blue);
+            }
+        }
+
+        private byte _Green 
+        { 
+            get
+            {
+                return Convert.ToByte(Value & 0x03E0);
+            }
+        }
+
+        public byte Green
+        {
+            get
+            {
+                return TextileToBitmapConverter.To32BPP(_Green);
+            }
+        }
+
+        private byte _Transparent 
+        { 
+            get
+            {
+                return Convert.ToByte(Value & 0x8000);
+            }
+        }
+
+        public byte Transparency
+        {
+            get
+            {
+                if (_Transparent == 0x1)
+                {
+                    return 0xFF;
+                }
+                else
+                {
+                    return 0x00;
+                }
+            }
+        }
+
+        public Textile16Pixel()
+        {
+            
         }
     }
 }
