bd9666e26f45a7daa052d4e468ef4e86138221fe
DanzaG <360danza@gmail.com>
2021-06-16 18:00:11 +0100

    #163 - TR3 empty classes

---
+++ TRLevelReader/Model/TR3/TR3SoundDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TRLevelReader.Serialization;
+
+namespace TRLevelReader.Model
+{
+    public class TR3SoundDetails : ISerializableCompact
+    {
+        public byte[] Serialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
