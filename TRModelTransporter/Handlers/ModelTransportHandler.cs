55333d4bb815ba528d33651dd6f79f2cdeb8fadd
lamb86 <lam8667@gmail.com>
2021-07-09 19:37:07 +0100

    #152 Randomized Outfits
    - Performance improvement to outfit randomization (levels now processed based on texture weighting).
    - Fixes a mesh problem in HSH during the start animation with Lara inspecting the dagger and the final shower cutscene. If Lara's outfit had been changed, her hips were still mapped to the dressing gown model. LaraMiscAnim is used here with a custom hips mesh, so a check is now down to remap this mesh to that of the  imported model.
    - Fixes a problem if viewing levels in trview where such things as camera targets, alarms, flame emitters etc were not rendering the hips placeholder model properly if the default outfit had changed.

--- TRModelTransporter/Handlers/ModelTransportHandler.cs
+++ TRModelTransporter/Handlers/ModelTransportHandler.cs
@@ -13,6 +13,15 @@ namespace TRModelTransporter.Handlers
             TR2Entities.LaraMiscAnim_H
         };
 
+        // These are models that use Lara's hips as placeholders - see Import
+        private static readonly List<TR2Entities> _laraDependentModels = new List<TR2Entities>
+        {
+            TR2Entities.CameraTarget_N, TR2Entities.FlameEmitter_N, TR2Entities.LaraCutscenePlacement_N,
+            TR2Entities.DragonExplosionEmitter_N, TR2Entities.BartoliHideoutClock_N, TR2Entities.SingingBirds_N,
+            TR2Entities.WaterfallMist_N, TR2Entities.DrippingWater_N, TR2Entities.LavaAirParticleEmitter_N,
+            TR2Entities.AlarmBell_N
+        };
+
         public TR2Entities ModelEntity { get; set; }
 
         public override void Export()
@@ -36,6 +45,23 @@ namespace TRModelTransporter.Handlers
                 // Replacement occurs for the likes of aliases taking the place of another
                 // e.g. WhiteTiger replacing BengalTiger in GW
                 Level.Models[i] = Definition.Model;
+            }
+
+            // If we have replaced Lara, we need to update models such as CameraTarget, FlameEmitter etc
+            // as these use Lara's hips as placeholders. This means we can avoid texture corruption in
+            // TRView but it's also needed for the shower cutscene in HSH. If these entities are found,
+            // their starting mesh and mesh tree indices are just remapped to Lara's.
+            if (Definition.Entity == TR2Entities.Lara)
+            {
+                foreach (TR2Entities dependent in _laraDependentModels)
+                {
+                    TRModel dependentModel = levelModels.Find(m => m.ID == (short)dependent);
+                    if (dependentModel != null)
+                    {
+                        dependentModel.MeshTree = Definition.Model.MeshTree;
+                        dependentModel.StartingMesh = Definition.Model.StartingMesh;
+                    }
+                }
             }
         }
     }
