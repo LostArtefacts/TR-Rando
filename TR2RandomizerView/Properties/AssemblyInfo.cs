84b9f04639d7e27a2ea49cdbb937e1b98f0ad092
DanzaG <360danza@gmail.com>
2021-07-25 13:53:53 +0100

    Updated version to 1.3.2

--- TR2RandomizerView/Properties/AssemblyInfo.cs
+++ TR2RandomizerView/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Windows;
 [assembly: AssemblyDescription("Tomb Raider II Randomizer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tombrunners")]
-[assembly: AssemblyProduct("TR2Rando V1.3.1")]
+[assembly: AssemblyProduct("TR2Rando V1.3.2")]
 [assembly: AssemblyCopyright("Copyright © Tombrunners 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
