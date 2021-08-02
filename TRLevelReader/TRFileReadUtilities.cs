eb957a967fafc94df2c9e5f8a38d53937a7aee43
DanzaG <360danza@gmail.com>
2021-08-02 18:42:12 +0100

    #162 - Initial TR1 level read and write

---
+++ TRLevelReader/TRFileReadUtilities.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TRLevelReader.Model;
+
+namespace TRLevelReader
+{
+    internal static class TRFileReadUtilities
+    {
+        public static TRRoomLight ReadRoomLight(BinaryReader reader)
+        {
+            return new TRRoomLight
+            {
+                X = reader.ReadInt32(),
+                Y = reader.ReadInt32(),
+                Z = reader.ReadInt32(),
+                Intensity = reader.ReadUInt16(),
+                Fade = reader.ReadUInt32(),
+            };
+        }
+
+        public static TRRoomStaticMesh ReadRoomStaticMesh(BinaryReader reader)
+        {
+            return new TRRoomStaticMesh
+            {
+                X = reader.ReadUInt32(),
+                Y = reader.ReadUInt32(),
+                Z = reader.ReadUInt32(),
+                Rotation = reader.ReadUInt16(),
+                Intensity = reader.ReadUInt16(),
+                MeshID = reader.ReadUInt16()
+            };
+        }
+
+        public static TRBox ReadBox(BinaryReader reader)
+        {
+            return new TRBox()
+            {
+                ZMin = reader.ReadUInt32(),
+                ZMax = reader.ReadUInt32(),
+                XMin = reader.ReadUInt32(),
+                XMax = reader.ReadUInt32(),
+                TrueFloor = reader.ReadInt16(),
+                OverlapIndex = reader.ReadUInt16()
+            };
+        }
+
+        public static TREntity ReadEntity(BinaryReader reader)
+        {
+            return new TREntity()
+            {
+                TypeID = reader.ReadInt16(),
+                Room = reader.ReadInt16(),
+                X = reader.ReadInt32(),
+                Y = reader.ReadInt32(),
+                Z = reader.ReadInt32(),
+                Angle = reader.ReadInt16(),
+                Intensity = reader.ReadInt16(),
+                Flags = reader.ReadUInt16()
+            };
+        }
+    }
+}
---
+++ TRLevelReaderUnitTests/TRLevel_UnitTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+using TRLevelReader;
+using TRLevelReader.Model;
+
+using TRFDControl;
+using TRFDControl.FDEntryTypes;
+using TRFDControl.Utilities;
+using System.Linq;
+
+namespace TRLevelReaderUnitTests
+{
+    [TestClass]
+    public class TRLevel_UnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
