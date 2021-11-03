5e5ac2c458a3f027936fb758988f2aac1ae8938a
lamb86 <lam8667@gmail.com>
2021-11-03 21:39:22 +0000

    #219 TR3 Environment Support
    Amends the vertex copying function to allow setting the attributes of all vertices in a room to the same value.

--- TREnvironmentEditor/Model/Types/Rooms/EMCopyVertexAttributesFunction.cs
+++ TREnvironmentEditor/Model/Types/Rooms/EMCopyVertexAttributesFunction.cs
@@ -8,6 +8,7 @@ namespace TREnvironmentEditor.Model.Types
     public class EMCopyVertexAttributesFunction : BaseEMFunction
     {
         public Dictionary<short, Dictionary<EMTextureFaceType, Dictionary<int, int>>> FaceMap { get; set; }
+        public Dictionary<short, TR3RoomVertex> RoomMap { get; set; }
 
         public override void ApplyToLevel(TR2Level level)
         {
@@ -16,39 +17,61 @@ namespace TREnvironmentEditor.Model.Types
 
         public override void ApplyToLevel(TR3Level level)
         {
-            foreach (short roomNumber in FaceMap.Keys)
+            // Allow changes per face
+            if (FaceMap != null)
             {
-                TR3Room room = level.Rooms[ConvertItemNumber(roomNumber, level.NumRooms)];
-                foreach (EMTextureFaceType faceType in FaceMap[roomNumber].Keys)
+                foreach (short roomNumber in FaceMap.Keys)
                 {
-                    foreach (int baseFaceIndex in FaceMap[roomNumber][faceType].Keys)
+                    TR3Room room = level.Rooms[ConvertItemNumber(roomNumber, level.NumRooms)];
+                    foreach (EMTextureFaceType faceType in FaceMap[roomNumber].Keys)
                     {
-                        ushort[] baseVertices, copyVertices;
-                        switch (faceType)
+                        foreach (int baseFaceIndex in FaceMap[roomNumber][faceType].Keys)
                         {
-                            case EMTextureFaceType.Rectangles:
-                                baseVertices = room.RoomData.Rectangles[baseFaceIndex].Vertices;
-                                copyVertices = room.RoomData.Rectangles[FaceMap[roomNumber][faceType][baseFaceIndex]].Vertices;
-                                break;
-                            case EMTextureFaceType.Triangles:
-                                baseVertices = room.RoomData.Triangles[baseFaceIndex].Vertices;
-                                copyVertices = room.RoomData.Triangles[FaceMap[roomNumber][faceType][baseFaceIndex]].Vertices;
-                                break;
-                            default:
-                                throw new ArgumentException();
-                        }
-                        
-                        for (int i = 0; i < baseVertices.Length; i++)
-                        {
-                            TR3RoomVertex baseVertex = room.RoomData.Vertices[baseVertices[i]];
-                            TR3RoomVertex copyVertex = room.RoomData.Vertices[copyVertices[i]];
-                            copyVertex.Attributes = baseVertex.Attributes;
-                            copyVertex.Colour = baseVertex.Colour;
-                            copyVertex.Lighting = baseVertex.Lighting;
+                            ushort[] baseVertices, copyVertices;
+                            switch (faceType)
+                            {
+                                case EMTextureFaceType.Rectangles:
+                                    baseVertices = room.RoomData.Rectangles[baseFaceIndex].Vertices;
+                                    copyVertices = room.RoomData.Rectangles[FaceMap[roomNumber][faceType][baseFaceIndex]].Vertices;
+                                    break;
+                                case EMTextureFaceType.Triangles:
+                                    baseVertices = room.RoomData.Triangles[baseFaceIndex].Vertices;
+                                    copyVertices = room.RoomData.Triangles[FaceMap[roomNumber][faceType][baseFaceIndex]].Vertices;
+                                    break;
+                                default:
+                                    throw new ArgumentException();
+                            }
+
+                            for (int i = 0; i < baseVertices.Length; i++)
+                            {
+                                TR3RoomVertex baseVertex = room.RoomData.Vertices[baseVertices[i]];
+                                TR3RoomVertex copyVertex = room.RoomData.Vertices[copyVertices[i]];
+                                CopyAttributes(baseVertex, copyVertex);
+                            }
                         }
                     }
                 }
             }
+
+            // Allow entire room changes
+            if (RoomMap != null)
+            {
+                foreach (short roomNumber in RoomMap.Keys)
+                {
+                    TR3Room room = level.Rooms[ConvertItemNumber(roomNumber, level.NumRooms)];
+                    foreach (TR3RoomVertex copyVertex in room.RoomData.Vertices)
+                    {
+                        CopyAttributes(RoomMap[roomNumber], copyVertex);
+                    }
+                }
+            }
+        }
+
+        private void CopyAttributes(TR3RoomVertex baseVertex, TR3RoomVertex copyVertex)
+        {
+            copyVertex.Attributes = baseVertex.Attributes;
+            copyVertex.Colour = baseVertex.Colour;
+            copyVertex.Lighting = baseVertex.Lighting;
         }
     }
 }
