0d5eafc62c40ca807bce554b5dc76a0fe78e259d
lahm86 <33758420+lahm86@users.noreply.github.com>
2022-08-26 09:46:23 +0100

    #352 TR1 Night Mode Randomization
    Ability to exclude rooms from having their lighting changed - currently targeted at the flipmap rooms at the start of Atlantis so we can retain the flicker effect here.

--- TRRandomizerCore/Randomizers/TR1/TR1NightModeRandomizer.cs
+++ TRRandomizerCore/Randomizers/TR1/TR1NightModeRandomizer.cs
@@ -14,6 +14,12 @@ namespace TRRandomizerCore.Randomizers
     {
         public const uint DarknessRange = 10; // 0 = Dusk, 10 = Night
 
+        private static readonly Dictionary<string, List<int>> _excludedRooms = new Dictionary<string, List<int>>
+        {
+            [TRLevelNames.ATLANTIS]
+                = new List<int> { 85, 95, 96 } // We want to retain the flicker effect at the start
+        };
+
         internal TR1TextureMonitorBroker TextureMonitor { get; set; }
 
         private List<TR1ScriptedLevel> _nightLevels;
@@ -57,12 +63,7 @@ namespace TRRandomizerCore.Randomizers
 
         private void SetNightMode(TR1CombinedLevel level)
         {
-            DarkenRooms(level.Data);
-
-            if (Settings.OverrideSunsets)
-            {
-                level.Script.HasSunset = false;
-            }
+            DarkenRooms(level);
 
             if (level.HasCutScene)
             {
@@ -74,15 +75,21 @@ namespace TRRandomizerCore.Randomizers
             monitor.UseNightTextures = true;
         }
 
-        private void DarkenRooms(TRLevel level)
+        private void DarkenRooms(TR1CombinedLevel level)
         {
             double scale = (100 - DarknessRange + Settings.NightModeDarkness) / 100d;
 
             short intensity1 = (short)(TR2Room.DarknessIntensity1 * scale);
             ushort intensity2 = (ushort)(TR2Room.DarknessIntensity2 * (2 - scale));
 
-            foreach (TRRoom room in level.Rooms)
+            for (int i = 0; i < level.Data.NumRooms; i++)
             {
+                if (_excludedRooms.ContainsKey(level.Name) && _excludedRooms[level.Name].Contains(i))
+                {
+                    continue;
+                }
+
+                TRRoom room = level.Data.Rooms[i];
                 room.SetAmbient(intensity1);
                 room.SetLights(intensity2);
                 room.SetStaticMeshLights((ushort)intensity1);
