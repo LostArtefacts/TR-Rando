819c6c5ff6634b60062fcc72a58625833fd03734
lahm86 <33758420+lahm86@users.noreply.github.com>
2022-09-10 15:30:05 +0100

    #350 T1M Settings
    Credit badge adjustment.

--- TRRandomizerCore/Editors/TR1RandoEditor.cs
+++ TRRandomizerCore/Editors/TR1RandoEditor.cs
@@ -325,7 +325,7 @@ namespace TRRandomizerCore.Editors
                 using (Bitmap badge = new Bitmap(@"Resources\Shared\Graphics\goldbadge-large.png"))
                 {
                     bg.Graphics.FillRectangle(new SolidBrush(Color.Black), new Rectangle(0, 0, 1920, 1080));
-                    bg.Graphics.DrawImage(badge, new Rectangle(768, 347, badge.Width, badge.Height));
+                    bg.Graphics.DrawImage(badge, new Rectangle(960 - badge.Width / 2, 540 - badge.Height / 2, badge.Width, badge.Height));
                     bg.Bitmap.Save(creditFile);
                 }
 
