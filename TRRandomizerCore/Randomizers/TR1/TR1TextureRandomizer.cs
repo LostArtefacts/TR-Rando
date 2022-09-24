d7f77fb2d1e3d0522c50935720bcfe7de5397ffb
lahm86 <33758420+lahm86@users.noreply.github.com>
2022-09-24 11:24:02 +0100

    #356 TR1 Texture Randomization
    Minimum water colours are now in place:
    - standard [0.1,0.1,0.1]
    - night-mode [0.2,0.2,0.2]
    - wireframe [0.3,0.3,0.3]
    - night + wireframe [0.4,0.4,0.4]

--- TRRandomizerCore/Randomizers/TR1/TR1TextureRandomizer.cs
+++ TRRandomizerCore/Randomizers/TR1/TR1TextureRandomizer.cs
@@ -274,10 +274,17 @@ namespace TRRandomizerCore.Randomizers
             }
             else
             {
+                int minValue = IsWireframeLevel(level) ? 30 : 10;
+                TextureMonitor<TREntities> monitor = TextureMonitor.GetMonitor(level.Name);
+                if (monitor != null && monitor.UseNightTextures)
+                {
+                    minValue += 10;
+                }
+
                 level.Script.WaterColor = new double[3];
                 for (int i = 0; i < 3; i++)
                 {
-                    level.Script.WaterColor[i] = Math.Round(_generator.Next(0, 101) * Math.Pow(10, -2), 2);
+                    level.Script.WaterColor[i] = Math.Round(_generator.Next(minValue, 101) * Math.Pow(10, -2), 2);
                 }
             }
         }
