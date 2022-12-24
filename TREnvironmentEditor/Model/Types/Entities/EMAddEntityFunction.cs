c2ec52127e56430b3fc2db78f94c718bafa04457
lahm86 <33758420+lahm86@users.noreply.github.com>
2022-12-24 20:51:42 +0000

    #360 TR1 Environment Randomization
    - Ability to specify a relocation for any existing entities when adding a new entity to a tile (e.g. if adding clang-clang doors, specify a relocation point for any pickups).
    - Caves environment mods expanded on with trap and puzzle additions.

--- TREnvironmentEditor/Model/Types/Entities/EMAddEntityFunction.cs
+++ TREnvironmentEditor/Model/Types/Entities/EMAddEntityFunction.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using TREnvironmentEditor.Helpers;
+using TRFDControl;
+using TRFDControl.Utilities;
 using TRLevelReader.Model;
 
 namespace TREnvironmentEditor.Model.Types
@@ -13,12 +15,32 @@ namespace TREnvironmentEditor.Model.Types
         public ushort Flags { get; set; }
         public short? Intensity { get; set; }
         public EMLocation Location { get; set; }
+        // If defined, anything else on the same tile will be moved here
+        public EMLocation TargetRelocation { get; set; }
 
         public override void ApplyToLevel(TRLevel level)
         {
             if (level.NumEntities < _defaultEntityLimit)
             {
                 EMLevelData data = GetData(level);
+                if (TargetRelocation != null)
+                {
+                    FDControl floorData = new FDControl();
+                    floorData.ParseFromLevel(level);
+                    short room = data.ConvertRoom(Location.Room);
+                    TRRoomSector sector = FDUtilities.GetRoomSector(Location.X, Location.Y, Location.Z, room, level, floorData);
+                    foreach (TREntity entity in level.Entities)
+                    {
+                        if (entity.Room == room && FDUtilities.GetRoomSector(entity.X, entity.Y, entity.Z, entity.Room, level, floorData) == sector)
+                        {
+                            entity.X = TargetRelocation.X;
+                            entity.Y = TargetRelocation.Y;
+                            entity.Z = TargetRelocation.Z;
+                            entity.Room = data.ConvertRoom(TargetRelocation.Room);
+                        }
+                    }
+                }
+
                 List<TREntity> entities = level.Entities.ToList();
                 entities.Add(CreateTREntity(data));
                 level.Entities = entities.ToArray();
@@ -31,6 +53,24 @@ namespace TREnvironmentEditor.Model.Types
             if (level.NumEntities < _defaultEntityLimit)
             {
                 EMLevelData data = GetData(level);
+                if (TargetRelocation != null)
+                {
+                    FDControl floorData = new FDControl();
+                    floorData.ParseFromLevel(level);
+                    short room = data.ConvertRoom(Location.Room);
+                    TRRoomSector sector = FDUtilities.GetRoomSector(Location.X, Location.Y, Location.Z, room, level, floorData);
+                    foreach (TR2Entity entity in level.Entities)
+                    {
+                        if (entity.Room == room && FDUtilities.GetRoomSector(entity.X, entity.Y, entity.Z, entity.Room, level, floorData) == sector)
+                        {
+                            entity.X = TargetRelocation.X;
+                            entity.Y = TargetRelocation.Y;
+                            entity.Z = TargetRelocation.Z;
+                            entity.Room = data.ConvertRoom(TargetRelocation.Room);
+                        }
+                    }
+                }
+
                 List<TR2Entity> entities = level.Entities.ToList();
                 entities.Add(CreateTR2Entity(data));
                 level.Entities = entities.ToArray();
@@ -43,6 +83,24 @@ namespace TREnvironmentEditor.Model.Types
             if (level.NumEntities < _defaultEntityLimit)
             {
                 EMLevelData data = GetData(level);
+                if (TargetRelocation != null)
+                {
+                    FDControl floorData = new FDControl();
+                    floorData.ParseFromLevel(level);
+                    short room = data.ConvertRoom(Location.Room);
+                    TRRoomSector sector = FDUtilities.GetRoomSector(Location.X, Location.Y, Location.Z, room, level, floorData);
+                    foreach (TR2Entity entity in level.Entities)
+                    {
+                        if (entity.Room == room && FDUtilities.GetRoomSector(entity.X, entity.Y, entity.Z, entity.Room, level, floorData) == sector)
+                        {
+                            entity.X = TargetRelocation.X;
+                            entity.Y = TargetRelocation.Y;
+                            entity.Z = TargetRelocation.Z;
+                            entity.Room = data.ConvertRoom(TargetRelocation.Room);
+                        }
+                    }
+                }
+
                 List<TR2Entity> entities = level.Entities.ToList();
                 entities.Add(CreateTR2Entity(data));
                 level.Entities = entities.ToArray();
