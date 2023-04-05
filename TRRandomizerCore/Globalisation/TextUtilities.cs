92462c57495797a99689fef39b381530705782c5
lahm86 <33758420+lahm86@users.noreply.github.com>
2023-04-05 19:10:27 +0100

    #359 Tidier Normalisation

--- TRRandomizerCore/Globalisation/TextUtilities.cs
+++ TRRandomizerCore/Globalisation/TextUtilities.cs
@@ -1,32 +1,21 @@
 ﻿using System.Globalization;
+using System.Linq;
 using System.Text;
 
 namespace TRRandomizerCore.Globalisation
 {
     public static class TextUtilities
     {
-        public static string Normalise(string s)
-        {
-            StringBuilder sb = new StringBuilder();
-            foreach (char c in s)
-            {
-                sb.Append(Normalise(c));
-            }
-            return sb.ToString();
-        }
-
         public static string Normalise(char c)
         {
-            StringBuilder sb = new StringBuilder();
-            string data = c.ToString().Normalize(NormalizationForm.FormD);
-            foreach (char d in data)
-            {
-                if (CharUnicodeInfo.GetUnicodeCategory(d) != UnicodeCategory.NonSpacingMark)
-                {
-                    sb.Append(d);
-                }
-            }
-            return sb.ToString();
+            return Normalise(c.ToString());
+        }
+
+        public static string Normalise(string s)
+        {
+            return new string(s.Normalize(NormalizationForm.FormD)
+                .Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
+                .ToArray());
         }
     }
 }
