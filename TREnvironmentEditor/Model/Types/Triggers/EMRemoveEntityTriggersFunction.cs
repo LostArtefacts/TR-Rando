1bc0fc7ec2f292f540cc42b109226b5e12ea933f
lahm86 <33758420+lahm86@users.noreply.github.com>
2023-06-02 09:46:26 +0100

    Clarify excluded types

--- TREnvironmentEditor/Model/Types/Triggers/EMRemoveEntityTriggersFunction.cs
+++ TREnvironmentEditor/Model/Types/Triggers/EMRemoveEntityTriggersFunction.cs
@@ -15,14 +15,15 @@ namespace TREnvironmentEditor.Model
         public override void ApplyToLevel(TRLevel level)
         {
             EMLevelData data = GetData(level);
-            List<int> entities = Entities.Select(e => (int)data.ConvertEntity(e)).ToList();
+            List<int> entities = GetEntities(data);
+            List<FDTrigType> excludedTypes = GetExcludedTypes();
 
             FDControl floorData = new FDControl();
             floorData.ParseFromLevel(level);
 
             foreach (TRRoom room in level.Rooms)
             {
-                RemoveSectorTriggers(floorData, room.Sectors, entities);
+                RemoveSectorTriggers(floorData, room.Sectors, entities, excludedTypes);
             }
 
             floorData.WriteToLevel(level);
@@ -31,14 +32,15 @@ namespace TREnvironmentEditor.Model
         public override void ApplyToLevel(TR2Level level)
         {
             EMLevelData data = GetData(level);
-            List<int> entities = Entities.Select(e => (int)data.ConvertEntity(e)).ToList();
+            List<int> entities = GetEntities(data);
+            List<FDTrigType> excludedTypes = GetExcludedTypes();
 
             FDControl floorData = new FDControl();
             floorData.ParseFromLevel(level);
 
             foreach (TR2Room room in level.Rooms)
             {
-                RemoveSectorTriggers(floorData, room.SectorList, entities);
+                RemoveSectorTriggers(floorData, room.SectorList, entities, excludedTypes);
             }
 
             floorData.WriteToLevel(level);
@@ -47,20 +49,41 @@ namespace TREnvironmentEditor.Model
         public override void ApplyToLevel(TR3Level level)
         {
             EMLevelData data = GetData(level);
-            List<int> entities = Entities.Select(e => (int)data.ConvertEntity(e)).ToList();
+            List<int> entities = GetEntities(data);
+            List<FDTrigType> excludedTypes = GetExcludedTypes();
 
             FDControl floorData = new FDControl();
             floorData.ParseFromLevel(level);
 
             foreach (TR3Room room in level.Rooms)
             {
-                RemoveSectorTriggers(floorData, room.Sectors, entities);
+                RemoveSectorTriggers(floorData, room.Sectors, entities, excludedTypes);
             }
 
             floorData.WriteToLevel(level);
         }
 
-        private void RemoveSectorTriggers(FDControl floorData, IEnumerable<TRRoomSector> sectors, List<int> entities)
+        private List<int> GetEntities(EMLevelData data)
+        {
+            List<int> entities = new List<int>();
+            if (Entities != null)
+            {
+                entities.AddRange(Entities.Select(e => (int)data.ConvertEntity(e)));
+            }
+            return entities;
+        }
+
+        private List<FDTrigType> GetExcludedTypes()
+        {
+            List<FDTrigType> types = new List<FDTrigType>();
+            if (ExcludedTypes != null)
+            {
+                types.AddRange(ExcludedTypes);
+            }
+            return types;
+        }
+
+        private static void RemoveSectorTriggers(FDControl floorData, IEnumerable<TRRoomSector> sectors, List<int> entities, List<FDTrigType> excludedTypes)
         {
             foreach (TRRoomSector sector in sectors)
             {
@@ -70,8 +93,9 @@ namespace TREnvironmentEditor.Model
                 }
 
                 List<FDEntry> entries = floorData.Entries[sector.FDIndex];
+                // Find any trigger that isn't a type we wish to exclude
                 if (entries.Find(e => e is FDTriggerEntry) is FDTriggerEntry trigger
-                    && (!ExcludedTypes?.Contains(trigger.TrigType) ?? true))
+                    && !excludedTypes.Contains(trigger.TrigType))
                 {
                     trigger.TrigActionList.RemoveAll(a => a.TrigAction == FDTrigAction.Object && entities.Contains(a.Parameter));
                     if (trigger.TrigActionList.Count == 0)
