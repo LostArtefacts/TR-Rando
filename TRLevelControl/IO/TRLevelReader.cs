1d65d5afc6d46dd0f8c3ae9e4a92d982fd6068f1
lahm86 <33758420+lahm86@users.noreply.github.com>
2023-08-11 17:29:23 +0100

    Create Image16 utilities

--- TRLevelControl/IO/TRLevelReader.cs
+++ TRLevelControl/IO/TRLevelReader.cs
@@ -7,6 +7,17 @@ public class TRLevelReader : BinaryReader
     public TRLevelReader(Stream stream)
         : base(stream) { }
 
+    public ushort[] ReadUInt16s(long numData)
+    {
+        ushort[] data = new ushort[numData];
+        for (int i = 0; i < numData; i++)
+        {
+            data[i] = ReadUInt16();
+        }
+
+        return data;
+    }
+
     public List<TRTexImage8> ReadImage8s(long numImages)
     {
         List<TRTexImage8> images = new((int)numImages);
@@ -15,6 +26,19 @@ public class TRLevelReader : BinaryReader
             images.Add(new()
             {
                 Pixels = ReadBytes(TRConsts.TPageSize)
+            });
+        }
+        return images;
+    }
+
+    public List<TRTexImage16> ReadImage16s(long numImages)
+    {
+        List<TRTexImage16> images = new((int)numImages);
+        for (long i = 0; i < numImages; i++)
+        {
+            images.Add(new()
+            {
+                Pixels = ReadUInt16s(TRConsts.TPageSize)
             });
         }
         return images;
--- TRLevelControl/IO/TRLevelWriter.cs
+++ TRLevelControl/IO/TRLevelWriter.cs
@@ -7,9 +7,25 @@ public class TRLevelWriter : BinaryWriter
     public TRLevelWriter(Stream stream)
         : base(stream) { }
 
+    public void Write(IEnumerable<ushort> data)
+    {
+        foreach (ushort value in data)
+        {
+            Write(value);
+        }
+    }
+
     public void Write(IEnumerable<TRTexImage8> images)
     {
         foreach (TRTexImage8 image in images)
+        {
+            Write(image.Pixels);
+        }
+    }
+
+    public void Write(IEnumerable<TRTexImage16> images)
+    {
+        foreach (TRTexImage16 image in images)
         {
             Write(image.Pixels);
         }
