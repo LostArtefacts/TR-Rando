d31627de2ade2ab4f72ff2c1c4148652dfc18fa9
lahm86 <33758420+lahm86@users.noreply.github.com>
2023-08-19 21:33:49 +0100

    Fix secret sector check
    Part of #510.

--- TREnvironmentEditor/Helpers/EMLocationUtilities.cs
+++ TREnvironmentEditor/Helpers/EMLocationUtilities.cs
@@ -18,16 +18,10 @@ public static class EMLocationUtilities
     public static int GetContainedSecretEntity(this EMLocation location, TR2Level level, FDControl floorData)
     {
         TRRoomSector sector = FDUtilities.GetRoomSector(location.X, location.Y, location.Z, location.Room, level, floorData);
-        for (int i = 0; i < level.NumEntities; i++)
-        {
-            TR2Entity entity = level.Entities[i];
-            if (TR2EntityUtilities.IsSecretType((TR2Entities)entity.TypeID)
-                && FDUtilities.GetRoomSector(location.X, location.Y, location.Z, location.Room, level, floorData) == sector)
-            {
-                return i;
-            }
-        }
-        return -1;
+        return Array.FindIndex(level.Entities, e =>
+            TR2EntityUtilities.IsSecretType((TR2Entities)e.TypeID)
+            && FDUtilities.GetRoomSector(e.X, e.Y, e.Z, e.Room, level, floorData) == sector
+        );
     }
 
     public static int GetContainedSecretEntity(this EMLocation location, TR3Level level, FDControl floorData)
