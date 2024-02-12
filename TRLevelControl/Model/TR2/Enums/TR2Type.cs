LostArtefacts/TR-Rando

commit 0d3989083a0fdcee60f15e8076ece60e93c424a7

Refactor and extend TR types (#547)
Part of #507.

TRLevelControl/Model/TR2/Enums/TR2Type.cs
@@ -1,0 +1,405 @@
+﻿namespace TRLevelControl.Model;
+
+//_S are sprites
+//_M are menu items
+
+//_P are pickup items
+//_N are nullmeshes (no render/collision)
+//_H are helper entities (not placed)
+//_U are unused entities
+public enum TR2Type
+{
+    Lara                     = 0,
+    LaraPistolAnim_H         = 1,
+    LaraPonytail_H           = 2,
+    LaraShotgunAnim_H        = 3,
+    LaraAutoAnim_H           = 4,
+    LaraUziAnim_H            = 5,
+    LaraM16Anim_H            = 6,
+    LaraGrenadeAnim_H        = 7,
+    LaraHarpoonAnim_H        = 8,
+    LaraFlareAnim_H          = 9,
+    LaraSnowmobAnim_H        = 10,
+    LaraBoatAnim_H           = 11,
+    LaraMiscAnim_H           = 12,
+    RedSnowmobile            = 13,
+    Boat                     = 14,
+    Doberman                 = 15,
+    MaskedGoon1              = 16,
+    MaskedGoon2              = 17,
+    MaskedGoon3              = 18,
+    Knifethrower             = 19,
+    ShotgunGoon              = 20,
+    Rat                      = 21,
+    DragonFront_H            = 22,
+    DragonBack_H             = 23,
+    Gondola                  = 24,
+    Shark                    = 25,
+    YellowMorayEel           = 26,
+    BlackMorayEel            = 27,
+    Barracuda                = 28,
+    ScubaDiver               = 29,
+    Gunman1                  = 30,
+    Gunman2                  = 31,
+    StickWieldingGoon1       = 32,
+    StickWieldingGoon2       = 33,
+    FlamethrowerGoon         = 34,
+    JellyFish_U              = 35,
+    Spider                   = 36,
+    GiantSpider              = 37,
+    Crow                     = 38,
+    TigerOrSnowLeopard       = 39,
+    MarcoBartoli             = 40,
+    XianGuardSpear           = 41,
+    XianGuardSpearStatue     = 42,
+    XianGuardSword           = 43,
+    XianGuardSwordStatue     = 44,
+    Yeti                     = 45,
+    BirdMonster              = 46,
+    Eagle                    = 47,
+    Mercenary1               = 48,
+    Mercenary2               = 49,
+    Mercenary3               = 50,
+    BlackSnowmob             = 51,
+    MercSnowmobDriver        = 52,
+    MonkWithLongStick        = 53,
+    MonkWithKnifeStick       = 54,
+    FallingBlock             = 55,
+    FallingBlock2            = 56,
+    LooseBoards              = 57,
+    SandbagOrBallsack        = 58,
+    TeethSpikesOrGlassShards = 59,
+    RollingBall              = 60,
+    Disc_H                   = 61,
+    Discgun                  = 62,
+    Drawbridge               = 63,
+    SlammingDoor             = 64,
+    Elevator                 = 65,
+    Minisub                  = 66,
+    PushBlock1               = 67,
+    PushBlock2               = 68,
+    PushBlock3               = 69,
+    PushBlock4               = 70,
+    LavaBowl                 = 71,
+    BreakableWindow1         = 72,
+    BreakableWindow2         = 73,
+    BreakableWindow3         = 74,
+    BreakableWindow4         = 75,
+    AirplanePropeller        = 76,
+    PowerSaw                 = 77,
+    OverheadPulleyHook       = 78,
+    FallingCeilingOrSandbag  = 79,
+    RollingSpindle           = 80,
+    WallMountedKnifeBlade    = 81,
+    StatueWithKnifeBlade     = 82,
+    BouldersOrSnowballs      = 83,
+    DetatchableIcicles       = 84,
+    SpikyWall                = 85,
+    BouncePad                = 86,
+    SpikyCeiling             = 87,
+    TibetanBell              = 88,
+    BoatWake_S_H             = 89,
+    SnowmobileWake_S_H       = 90,
+    SnowmobileBelt           = 91,
+    WheelKnob                = 92,
+    SmallWallSwitch          = 93,
+    UnderwaterPropeller      = 94,
+    AirFan                   = 95,
+    SwingingBoxOrBall        = 96,
+    MeshSwap1                = 97,
+    MeshSwap2                = 98,
+    MeshSwap3                = 99,
+    UIFrame_H                = 100,
+    RollingStorageDrums      = 101,
+    ZiplineHandle            = 102,
+    PushButtonSwitch         = 103,
+    WallSwitch               = 104,
+    UnderwaterSwitch         = 105,
+    Door1                    = 106,
+    Door2                    = 107,
+    Door3                    = 108,
+    Door4                    = 109,
+    Door5                    = 110,
+    LiftingDoor1             = 111,
+    LiftingDoor2             = 112,
+    LiftingDoor3             = 113,
+    Trapdoor1                = 114,
+    Trapdoor2                = 115,
+    Trapdoor3                = 116,
+    BridgeFlat               = 117,
+    BridgeTilt1              = 118,
+    BridgeTilt2              = 119,
+    JadeSecret_M_H           = 120,
+    SilverSecret_M_H         = 121,
+    LaraHomePhoto_M_H        = 122,
+    CutsceneActor1           = 123,
+    CutsceneActor2           = 124,
+    CutsceneActor3           = 125,
+    CutsceneActor4           = 126,
+    CutsceneActor5           = 127,
+    CutsceneActor6           = 128,
+    CutsceneActor7           = 129,
+    CutsceneActor8           = 130,
+    CutsceneActor9           = 131,
+    CutsceneActor10          = 132,
+    PassportClosed_M_H       = 133,
+    Map_M_U                  = 134,
+    Pistols_S_P              = 135,
+    Shotgun_S_P              = 136,
+    Automags_S_P             = 137,
+    Uzi_S_P                  = 138,
+    Harpoon_S_P              = 139,
+    M16_S_P                  = 140,
+    GrenadeLauncher_S_P      = 141,
+    PistolAmmo_S_P           = 142,
+    ShotgunAmmo_S_P          = 143,
+    AutoAmmo_S_P             = 144,
+    UziAmmo_S_P              = 145,
+    HarpoonAmmo_S_P          = 146,
+    M16Ammo_S_P              = 147,
+    Grenades_S_P             = 148,
+    SmallMed_S_P             = 149,
+    LargeMed_S_P             = 150,
+    Flares_S_P               = 151,
+    Flare_H                  = 152,
+    Sunglasses_M_H           = 153,
+    CDPlayer_M_H             = 154,
+    DirectionKeys_M_H        = 155,
+    Gamme_M_H                = 156,
+    Pistols_M_H              = 157,
+    Shotgun_M_H              = 158,
+    Autos_M_H                = 159,
+    Uzi_M_H                  = 160,
+    Harpoon_M_H              = 161,
+    M16_M_H                  = 162,
+    GrenadeLauncher_M_H      = 163,
+    PistolAmmo_M_H           = 164,
+    ShotgunAmmo_M_H          = 165,
+    AutoAmmo_M_H             = 166,
+    UziAmmo_M_H              = 167,
+    HarpoonAmmo_M_H          = 168,
+    M16Ammo_M_H              = 169,
+    Grenades_M_H             = 170,
+    SmallMed_M_H             = 171,
+    LargeMed_M_H             = 172,
+    Flares_M_H               = 173,
+    Puzzle1_S_P              = 174,
+    Puzzle2_S_P              = 175,
+    Puzzle3_S_P              = 176,
+    Puzzle4_S_P              = 177,
+    Puzzle1_M_H              = 178,
+    Puzzle2_M_H              = 179,
+    Puzzle3_M_H              = 180,
+    Puzzle4_M_H              = 181,
+    PuzzleHole1              = 182,
+    PuzzleHole2              = 183,
+    PuzzleHole3              = 184,
+    PuzzleHole4              = 185,
+    PuzzleDone1              = 186,
+    PuzzleDone2              = 187,
+    PuzzleDone3              = 188,
+    PuzzleDone4              = 189,
+    GoldSecret_S_P           = 190,
+    JadeSecret_S_P           = 191,
+    StoneSecret_S_P          = 192,
+    Key1_S_P                 = 193,
+    Key2_S_P                 = 194,
+    Key3_S_P                 = 195,
+    Key4_S_P                 = 196,
+    Key1_M_H                 = 197,
+    Key2_M_H                 = 198,
+    Key3_M_H                 = 199,
+    Key4_M_H                 = 200,
+    Keyhole1                 = 201,
+    Keyhole2                 = 202,
+    Keyhole3                 = 203,
+    Keyhole4                 = 204,
+    Quest1_S_P               = 205,
+    Quest2_S_P               = 206,
+    Quest1_M_H               = 207,
+    Quest2_M_H               = 208,
+    DragonExplosion1_H       = 209,
+    DragonExplosion2_H       = 210,
+    DragonExplosion3_H       = 211,
+    Alarm_N                  = 212,
+    DrippingWater_N          = 213,
+    TRex                     = 214,
+    SingingBirds_N           = 215,
+    BartoliHideoutClock_N    = 216,
+    Placeholder_N            = 217,
+    DragonBonesFront_H       = 218,
+    DragonBonesBack_H        = 219,
+    ExtraFire_S_H            = 220,
+    BoatBits                 = 221,
+    AquaticMine              = 222,
+    MenuBackground_H         = 223,
+    GrayDisk_S_H             = 224,
+    GongStick_H              = 225,
+    Gong                     = 226,
+    DetonatorBox             = 227,
+    Helicopter               = 228,
+    Explosion_S_H            = 229,
+    WaterRipples_S_H         = 230,
+    Bubbles1_S_H             = 231,
+    Bubbles2_S_H             = 232,
+    Blood_S_H                = 233,
+    DartEffect_S_H           = 234,
+    FlareSparks_S_H          = 235,
+    Glow_S_H                 = 236,
+    MapGlow_S_H              = 237,
+    Ricochet_S_H             = 238,
+    XianGuardSparkles_S_H    = 239, // Shows when XianGuardSword is hovering
+    Gunflare_H               = 240,
+    M16Gunflare_H            = 241,
+    BodyPart                 = 242,
+    CameraTarget_N           = 243,
+    WaterfallMist_N          = 244,
+    ScubaHarpoonProjectile_H = 245, // A repeat of HarpoonProjectile_H
+    FireBlast_S_H            = 246, // A repeat of Explosion_S_H, used by Dragon and Flamethrower
+    KnifeProjectile_H        = 247, // Thrown by Knifethrower in Floater/DL
+    GrenadeProjectile_H      = 248,
+    HarpoonProjectile_H      = 249,
+    LavaParticles_S_H        = 250,
+    LavaAirParticleEmitter_N = 251,
+    Flame_S_H                = 252,
+    FlameEmitter_N           = 253,
+    Skybox_H                 = 254,
+    FontGraphics_S_H         = 255,
+    Monk                     = 256,
+    DoorBell_N               = 257,
+    AlarmBell_N              = 258,
+    Helicopter2              = 259,
+    Winston                  = 260,
+    AssaultNumbers           = 261,
+    LaraCutscenePlacement_N  = 262,
+    ShotgunShowerAnimation_H = 263,
+    DragonExplosionEmitter_N = 264,
+
+    // Scenery
+    Plant0                   = 265,
+    Plant1                   = 266,
+    Plant2                   = 267,
+    Plant3                   = 268,
+    Plant4                   = 269,
+    Plant5                   = 270,
+    Plant6                   = 271,
+    Plant7                   = 272,
+    Plant8                   = 273,
+    Plant9                   = 274,
+    Furniture0               = 275,
+    Furniture1               = 276,
+    Furniture2               = 277,
+    Furniture3               = 278,
+    Furniture4               = 279,
+    Furniture5               = 280,
+    Furniture6               = 281,
+    Furniture7               = 282,
+    Furniture8               = 283,
+    Furniture9               = 284,
+    Rock0                    = 285,
+    Rock1                    = 286,
+    Rock2                    = 287,
+    Rock3                    = 288,
+    Rock4                    = 289,
+    Rock5                    = 290,
+    Rock6                    = 291,
+    Rock7                    = 292,
+    Rock8                    = 293,
+    Rock9                    = 294,
+    Architecture0            = 295,
+    Architecture1            = 296,
+    Architecture2            = 297,
+    Architecture3            = 298,
+    Architecture4            = 299,
+    Architecture5            = 300,
+    Architecture6            = 301,
+    Architecture7            = 302,
+    Architecture8            = 303,
+    Architecture9            = 304,
+    Debris0                  = 305,
+    Debris1                  = 306,
+    Debris2                  = 307,
+    Debris3                  = 308,
+    Debris4                  = 309,
+    Debris5                  = 310,
+    Debris6                  = 311,
+    Debris7                  = 312,
+    Debris8                  = 313,
+    Debris9                  = 314,
+    SceneryBase              = Plant0,
+
+    // Alias entries
+    BengalTiger                   = 1000,
+    SnowLeopard                   = 1001,
+    WhiteTiger                    = 1002,
+    StickWieldingGoon1Bandana     = 1003,
+    StickWieldingGoon1BlackJacket = 1004,
+    StickWieldingGoon1BodyWarmer  = 1005,
+    StickWieldingGoon1GreenVest   = 1006,
+    StickWieldingGoon1WhiteVest   = 1007,
+    BarracudaUnwater              = 1008, // 40F, MD, LQ, Deck
+    BarracudaIce                  = 1009, // CoT, Ice Palace
+    BarracudaXian                 = 1010, // Xian
+
+    Gunman1OG                     = 1011,
+    Gunman1TopixtorORC            = 1012,
+    Gunman1TopixtorCAC            = 1013,
+    FlamethrowerGoonOG            = 1014,
+    FlamethrowerGoonTopixtor      = 1015,
+
+    // Extra Lara animations
+    LaraMiscAnim_H_Ice            = 2000, // Death-by-Yeti, Gong action
+    LaraMiscAnim_H_Unwater        = 2001, // Death-by-Shark, opening wheel door
+    LaraMiscAnim_H_Xian           = 2002, // Death-by-Guard, inspecting dagger
+    LaraMiscAnim_H_Wall           = 2003, // Death-by-Barney
+    LaraMiscAnim_H_HSH            = 2004, // Inspecting dagger at home
+    LaraMiscAnim_H_Venice         = 2005, // Bartoli's detonator
+
+    // Split Lara's outfits and weapon animations
+    LaraSun                       = 3000,
+    LaraPistolAnim_H_Sun          = 3001,
+    LaraAutoAnim_H_Sun            = 3002,
+    LaraUziAnim_H_Sun             = 3003,
+
+    LaraUnwater                   = 3100,
+    LaraPistolAnim_H_Unwater      = 3101,
+    LaraAutoAnim_H_Unwater        = 3102,
+    LaraUziAnim_H_Unwater         = 3103,
+
+    LaraSnow                      = 3200,
+    LaraPistolAnim_H_Snow         = 3201,
+    LaraAutoAnim_H_Snow           = 3202,
+    LaraUziAnim_H_Snow            = 3203,
+
+    LaraHome                      = 3300,
+    LaraPistolAnim_H_Home         = 3301,
+    LaraAutoAnim_H_Home           = 3302,
+    LaraUziAnim_H_Home            = 3303,
+
+    LaraInvisible                 = 3400,
+
+    // Puzzle and quest aliases
+    Puzzle1_M_H_RelayBox          = 3500,
+    Puzzle2_M_H_CircuitBoard      = 3501,
+
+    Puzzle1_M_H_CircuitBoard      = 3502,
+
+    Puzzle1_M_H_CircuitBreaker    = 3503,
+
+    Puzzle4_M_H_Seraph            = 3504,
+
+    Puzzle1_M_H_PrayerWheel       = 3505,
+    Puzzle2_M_H_GemStone          = 3506,
+
+    Puzzle1_M_H_TibetanMask       = 3507,
+    Quest1_M_H_GongHammer         = 3508,
+    Quest2_M_H_GongHammer         = 3509,
+
+    Puzzle1_M_H_DragonSeal        = 3510,
+
+    Puzzle1_M_H_MysticPlaque      = 3511,
+    Puzzle2_M_H_MysticPlaque      = 3512,
+
+    Puzzle1_M_H_Dagger            = 3513,
+    Puzzle2_M_H_Dagger            = 3514,
+}

commit 8a27726e6c10f29ce4ac93b65ed109dbbd0f6800

Fix TR2Type mismatches
Resolves #605.
Regression from #547.

TRLevelControl/Model/TR2/Enums/TR2Type.cs
@@ -106,9 +106,9 @@ public enum TR2Type
     UnderwaterPropeller      = 94,
     AirFan                   = 95,
     SwingingBoxOrBall        = 96,
-    MeshSwap1                = 97,
-    MeshSwap2                = 98,
-    MeshSwap3                = 99,
+    CutsceneActor1           = 97,
+    CutsceneActor2           = 98,
+    CutsceneActor3           = 99,
     UIFrame_H                = 100,
     RollingStorageDrums      = 101,
     ZiplineHandle            = 102,
@@ -132,16 +132,16 @@ public enum TR2Type
     JadeSecret_M_H           = 120,
     SilverSecret_M_H         = 121,
     LaraHomePhoto_M_H        = 122,
-    CutsceneActor1           = 123,
-    CutsceneActor2           = 124,
-    CutsceneActor3           = 125,
-    CutsceneActor4           = 126,
-    CutsceneActor5           = 127,
-    CutsceneActor6           = 128,
-    CutsceneActor7           = 129,
-    CutsceneActor8           = 130,
-    CutsceneActor9           = 131,
-    CutsceneActor10          = 132,
+    CutsceneActor4           = 123,
+    CutsceneActor5           = 124,
+    CutsceneActor6           = 125,
+    CutsceneActor7           = 126,
+    CutsceneActor8           = 127,
+    CutsceneActor9           = 128,
+    CutsceneActor10          = 129,
+    CutsceneActor11          = 130,
+    Unknown1                 = 131,
+    Unknown2                 = 132,
     PassportClosed_M_H       = 133,
     Map_M_U                  = 134,
     Pistols_S_P              = 135,