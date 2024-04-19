164ba809b3a714a5ff9a686a3a58dbed326f6efa
lahm86 <33758420+lahm86@users.noreply.github.com>
2024-04-19 15:05:30 +0100

    Make common definitions

--- TRRandomizerCore/SFX/TRSFXDefinition.cs
+++ TRRandomizerCore/SFX/TRSFXDefinition.cs
@@ -1,16 +1,20 @@
 ﻿using Newtonsoft.Json;
-using System.Text;
+using TRLevelControl.Model;
 
 namespace TRRandomizerCore.SFX;
 
-public class TRSFXDefinition<D> where D : class
+public class TRSFXDefinition<T, E>
+    where T : Enum
+    where E : class
 {
-    public short InternalIndex { get; set; }
+    public T InternalIndex { get; set; }
     public string Description { get; set; }
     public TRSFXCreatureCategory Creature { get; set; }
     public List<TRSFXGeneralCategory> Categories { get; set; }
-    public D Details { get; set; }
-    public List<uint> SampleIndices { get; set; }
+    public string SourceLevel { get; set; }
+
+    [JsonIgnore]
+    public E SoundEffect { get; set; }
 
     [JsonIgnore]
     public TRSFXGeneralCategory PrimaryCategory
@@ -20,16 +24,8 @@ public class TRSFXDefinition<D> where D : class
             return Categories.Count > 0 ? Categories[0] : TRSFXGeneralCategory.Misc;
         }
     }
-
-    public override string ToString()
-    {
-        StringBuilder sb = new(base.ToString());
-
-        sb.Append(" ID: " + InternalIndex);
-        sb.Append(" Description: " + Description);
-        sb.Append(" Categories: [" + string.Join(", ", Categories) + "]");
-        sb.Append(" Creature: " + Creature);
-
-        return sb.ToString();
-    }
 }
+
+public class TR1SFXDefinition : TRSFXDefinition<TR1SFX, TR1SoundEffect> { }
+public class TR2SFXDefinition : TRSFXDefinition<TR2SFX, TR2SoundEffect> { }
+public class TR3SFXDefinition : TRSFXDefinition<TR3SFX, TR3SoundEffect> { }
