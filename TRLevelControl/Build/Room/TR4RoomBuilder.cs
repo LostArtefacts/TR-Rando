aa326fa809fd62a19d8ca3b86595367cc00afbb5
lahm86 <33758420+lahm86@users.noreply.github.com>
2024-04-30 12:02:45 +0100

    Standardise TR4 room mesh

---
+++ TRLevelControl/Build/Room/TR4RoomBuilder.cs
@@ -0,0 +1,38 @@
+﻿using TRLevelControl.Model;
+
+namespace TRLevelControl.Build;
+
+public class TR4RoomBuilder : TRRoomBuilder<TR3RoomVertex>
+{
+    public TR4RoomBuilder()
+        : base(TRGameVersion.TR4) { }
+
+    protected override List<TR3RoomVertex> ReadVertices(TRLevelReader reader)
+    {
+        short numVertices = reader.ReadInt16();
+        List<TR3RoomVertex> vertices = new();
+        for (int i = 0; i < numVertices; i++)
+        {
+            vertices.Add(new()
+            {
+                Vertex = reader.ReadVertex(),
+                Lighting = reader.ReadInt16(),
+                Attributes = reader.ReadUInt16(),
+                Colour = reader.ReadUInt16(),
+            });
+        }
+        return vertices;
+    }
+
+    protected override void WriteVertices(TRLevelWriter writer, List<TR3RoomVertex> vertices)
+    {
+        writer.Write((short)vertices.Count);
+        foreach (TR3RoomVertex vertex in vertices)
+        {
+            writer.Write(vertex.Vertex);
+            writer.Write(vertex.Lighting);
+            writer.Write(vertex.Attributes);
+            writer.Write(vertex.Colour);
+        }
+    }
+}
--- TRLevelControl/Model/TR3/TR3RoomStaticMesh.cs
+++ TRLevelControl/Model/TR3/TR3RoomStaticMesh.cs
@@ -1,37 +1,12 @@
-﻿using TRLevelControl.Serialization;
+﻿namespace TRLevelControl.Model;
 
-namespace TRLevelControl.Model;
-
-public class TR3RoomStaticMesh : ISerializableCompact
+public class TR3RoomStaticMesh
 {
     public uint X { get; set; }
-
     public uint Y { get; set; }
-
     public uint Z { get; set; }
-
     public ushort Rotation { get; set; }
-
     public ushort Colour { get; set; }
-
     public ushort Unused { get; set; }
-
     public ushort MeshID { get; set; }
-
-    public byte[] Serialize()
-    {
-        using MemoryStream stream = new();
-        using (BinaryWriter writer = new(stream))
-        {
-            writer.Write(X);
-            writer.Write(Y);
-            writer.Write(Z);
-            writer.Write(Rotation);
-            writer.Write(Colour);
-            writer.Write(Unused);
-            writer.Write(MeshID);
-        }
-
-        return stream.ToArray();
-    }
 }
