6f35851d3cb17fc5da7c5129de193c4be26bcefc
lahm86 <33758420+lahm86@users.noreply.github.com>
2024-05-03 21:10:14 +0100

    Remove stale Serialize methods

--- TRLevelControl/Model/TR3/TR3RoomSpotlight.cs
+++ TRLevelControl/Model/TR3/TR3RoomSpotlight.cs
@@ -1,22 +1,7 @@
-﻿using TRLevelControl.Serialization;
+﻿namespace TRLevelControl.Model;
 
-namespace TRLevelControl.Model;
-
-public class TR3RoomSpotlight : ISerializableCompact
+public class TR3RoomSpotlight
 {
     public int Intensity { get; set; }
-
     public int Fade { get; set; }
-
-    public byte[] Serialize()
-    {
-        using MemoryStream stream = new();
-        using (BinaryWriter writer = new(stream))
-        {
-            writer.Write(Intensity);
-            writer.Write(Fade);
-        }
-
-        return stream.ToArray();
-    }
 }
--- TRLevelControl/Model/TR3/TR3RoomSun.cs
+++ TRLevelControl/Model/TR3/TR3RoomSun.cs
@@ -1,28 +1,9 @@
-﻿using TRLevelControl.Serialization;
+﻿namespace TRLevelControl.Model;
 
-namespace TRLevelControl.Model;
-
-public class TR3RoomSun : ISerializableCompact
+public class TR3RoomSun
 {
     public short NormalX { get; set; }
-
     public short NormalY { get; set; }
-
     public short NormalZ { get; set; }
-
     public short Unused { get; set; }
-
-    public byte[] Serialize()
-    {
-        using MemoryStream stream = new();
-        using (BinaryWriter writer = new(stream))
-        {
-            writer.Write(NormalX);
-            writer.Write(NormalY);
-            writer.Write(NormalZ);
-            writer.Write(Unused);
-        }
-
-        return stream.ToArray();
-    }
 }
