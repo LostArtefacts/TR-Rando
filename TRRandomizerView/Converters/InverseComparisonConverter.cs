a8f3eaf9364999fc6b49a23b0be07ee25043affb
lahm86 <33758420+lahm86@users.noreply.github.com>
2024-07-01 19:46:59 +0100

    Update UI

---
+++ TRRandomizerView/Converters/InverseComparisonConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TRRandomizerView.Converters;
+
+internal class InverseComparisonConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return !value?.Equals(parameter);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value?.Equals(true) == true ? Binding.DoNothing : parameter;
+    }
+}
