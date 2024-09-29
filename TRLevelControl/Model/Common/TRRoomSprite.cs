6fa767874dfd0342df9458b5ab03f13cf888f35b
lahm86 <33758420+lahm86@users.noreply.github.com>
2024-09-29 13:11:58 +0100

    Fix room sprite offset handling
    Resolves #770.

--- TRLevelControl/Model/Common/TRRoomSprite.cs
+++ TRLevelControl/Model/Common/TRRoomSprite.cs
@@ -4,6 +4,7 @@ public class TRRoomSprite<T> : ICloneable
     where T : Enum
 {
     public T ID { get; set; }
+    public short Frame { get; set; }
     public short Vertex { get; set; }
 
     public TRRoomSprite<T> Clone()
