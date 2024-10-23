81d3da2340b453ce71bd5c2b5f54dd1b581a9bfc
lahm86 <33758420+lahm86@users.noreply.github.com>
2024-10-23 12:52:46 +0100

    Move randomizer core tests
    This moves the tests that were previously in ImageControl and LevelControl into a randomizer core specific test library.

---
+++ TRRandomizerCoreTests/BitmapTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using TRImageControl;
+using TRRandomizerCore.Utilities;
+
+namespace TRRandomizerCoreTests;
+
+[TestClass]
+[TestCategory("Bitmaps")]
+public class BitmapTests
+{
+    [TestMethod]
+    [Description("Test bitmap interpretation.")]
+    public void TestBitmap()
+    {
+        using Bitmap bitmap1 = new(64, 32);
+        Graphics graphics = Graphics.FromImage(bitmap1);
+
+        graphics.FillRectangle(new SolidBrush(Color.Blue), new(0, 0, 64, 16));
+        graphics.FillRectangle(new SolidBrush(Color.Green), new(0, 16, 64, 16));
+        graphics.DrawLine(new(Color.Red, 1), new(10, 10), new(20, 20));
+
+        using MemoryStream ms1 = new();
+        bitmap1.Save(ms1, ImageFormat.Png);
+
+        TRImage image = ImageUtilities.BitmapToImage(bitmap1);
+        Bitmap bitmap2 = ImageUtilities.ImageToBitmap(image);
+
+        using MemoryStream ms2 = new();
+        bitmap2.Save(ms2, ImageFormat.Png);
+
+        CollectionAssert.AreEqual(ms1.ToArray(), ms2.ToArray());
+    }
+}
