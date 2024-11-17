3275d48a99e274776884fbdbc61c045d1fdf18a9
lahm86 <33758420+lahm86@users.noreply.github.com>
2024-11-17 10:30:26 +0000

    Fix lambda compilation warnings
    Fixes warnings about lamba compilation time.

--- TRLevelControl/Model/Common/TRModel.cs
+++ TRLevelControl/Model/Common/TRModel.cs
@@ -6,6 +6,11 @@ public class TRModel : ICloneable
     public List<TRMeshTreeNode> MeshTrees { get; set; } = new();
     public List<TRMesh> Meshes { get; set; } = new();
 
+    public int TotalCommandCount => Animations.Sum(a => a.Commands.Count);
+    public int TotalChangeCount => Animations.Sum(a => a.Changes.Count);
+    public int TotalDispatchCount => Animations.Sum(a => a.TotalDispatchCount);
+    public int TotalFrameCount => Animations.Sum(a => a.Frames.Count);
+
     public TRModel Clone()
     {
         return new()
