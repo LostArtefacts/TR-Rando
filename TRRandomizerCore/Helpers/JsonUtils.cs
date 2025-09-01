56ef965dd15bda3604c11aba7aeb7d9450cf0e36
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-09-01 10:02:26 +0100

    Move texture deduplication
    Moves TR2 texture deduplication into pre-processing.

---
+++ TRRandomizerCore/Helpers/JsonUtils.cs
@@ -0,0 +1,9 @@
+﻿using Newtonsoft.Json;
+
+namespace TRRandomizerCore.Helpers;
+
+public static class JsonUtils
+{
+    public static T ReadFile<T>(string filePath)
+        => JsonConvert.DeserializeObject<T>(File.ReadAllText(filePath));
+}
---
+++ TRRandomizerCore/Processors/TR2/Tasks/TR2XDeduplicationTask.cs
@@ -0,0 +1,49 @@
+﻿using TRDataControl;
+using TRImageControl.Packing;
+using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRModelTransporter.Utilities;
+using TRRandomizerCore.Helpers;
+using TRRandomizerCore.Levels;
+
+namespace TRRandomizerCore.Processors.TR2.Tasks;
+
+public class TR2XDeduplicationTask : ITR2ProcessorTask
+{
+    public void Run(TR2CombinedLevel level)
+    {
+        if (level.Is(TR2LevelNames.FLOATER) && level.Data.AnimatedTextures[0].Textures[0] == 1702)
+        {
+            level.IsUKBox = true;
+        }
+
+        var dedupPath = $"Resources/TR2/Textures/Deduplication/{level.JsonID}-TextureRemap.json";
+        if (!File.Exists(dedupPath))
+        {
+            return;
+        }
+
+        var levelPacker = new TR2TexturePacker(level.Data);
+        var allTextures = new Dictionary<TRTextile, List<TRTextileRegion>>();
+        foreach (var tile in levelPacker.Tiles)
+        {
+            allTextures[tile] = [.. tile.Rectangles];
+        }
+
+        var remapGroup = JsonUtils.ReadFile<TR2TextureRemapGroup>(dedupPath);
+        var deduplicator = new TRTextureDeduplicator<TR2Type>
+        {
+            UpdateGraphics = true,
+            SegmentMap = allTextures,
+            PrecompiledRemapping = remapGroup.Remapping,
+        };
+
+        deduplicator.Deduplicate();
+
+        levelPacker.AllowEmptyPacking = true;
+        levelPacker.Pack(true);
+
+        var remapper = new TR2TextureRemapper(level.Data);
+        remapper.ResetUnusedTextures();
+    }
+}
