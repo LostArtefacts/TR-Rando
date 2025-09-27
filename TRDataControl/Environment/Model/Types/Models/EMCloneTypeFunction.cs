fbcb7ec5e771106897ca07132fbd4e95377e7b71
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-09-27 10:00:48 +0100

    Alter Ice Palace ending
    Alters the end of Ice Palace if the chicken isn't there so that the enemy drops a puzzle 4 item rather than puzzle 1. Avoids being able to use the other puzzle 1 item.

---
+++ TRDataControl/Environment/Model/Types/Models/EMCloneTypeFunction.cs
@@ -0,0 +1,45 @@
+﻿using TRLevelControl.Model;
+
+namespace TRDataControl.Environment;
+
+public class EMCloneTypeFunction : BaseEMFunction
+{
+    public Dictionary<uint, uint> ModelMap { get; set; }
+    public Dictionary<uint, uint> SpriteMap { get; set; }
+
+    public override void ApplyToLevel(TR1Level level)
+        => Clone(level.Models, level.Sprites);
+
+    public override void ApplyToLevel(TR2Level level)
+        => Clone(level.Models, level.Sprites);
+
+    public override void ApplyToLevel(TR3Level level)
+        => Clone(level.Models, level.Sprites);
+
+    private void Clone<T>(TRDictionary<T, TRModel> models, TRDictionary<T, TRSpriteSequence> sprites)
+        where T : Enum
+    {
+        Clone(models, ModelMap);
+        Clone(sprites, SpriteMap);
+    }
+
+    private static void Clone<T, V>(TRDictionary<T, V> levelMap, Dictionary<uint, uint> cloneMap)
+        where T : Enum
+        where V : class, ICloneable
+    {
+        if (cloneMap == null)
+        {
+            return;
+        }
+
+        foreach (var (baseId, targetId) in cloneMap)
+        {
+            var baseType = (T)(object)baseId;
+            var targetType = (T)(object)targetId;
+            if (levelMap.TryGetValue(baseType, out var value))
+            {
+                levelMap[targetType] = (V)value.Clone();
+            }
+        }
+    }
+}
